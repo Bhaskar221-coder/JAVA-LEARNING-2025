71b1cb3f900d510896a5390117f190d8435a7baa
Bhaskarcoder <Bhaskertec123@gmail.com>
2025-08-29 21:19:09 +0530

    lesson 1 readme added and some changes done

--- FirstProject/src/lesson1/MyClass.java
+++ FirstProject/src/lesson1/MyClass.java
@@ -3,6 +3,6 @@ package lesson1;
 public class MyClass {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! This is my first Java program.");
     }
 }
