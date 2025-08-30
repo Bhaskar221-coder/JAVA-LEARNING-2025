530dbd8fc3b760e53cc1b023c48b3dbe2335ab6c
Bhaskarcoder <Bhaskertec123@gmail.com>
2025-08-30 18:54:31 +0530

    lession 1 commits

---
+++ FirstProject/src/lesson1/DataType.java
@@ -0,0 +1,14 @@
+package lesson1;
+public class DataType {
+    public static void main(String[] args) {
+      short a = 5;
+      byte b= 10;
+      boolean c = true;
+      long d = 20L;
+      float e = 25.5F;
+      double f = 30.99;
+      int m = b + a;
+      float n = m + e;
+      System.out.println(n);
+   }
+}
