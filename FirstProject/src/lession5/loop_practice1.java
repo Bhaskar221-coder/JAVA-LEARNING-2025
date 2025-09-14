68f73ec98312d05ee7999e0cae3b7cc154384039
Bhaskarcoder <Bhaskertec123@gmail.com>
2025-09-15 00:22:08 +0530

    loop practice commit

---
+++ FirstProject/src/lession5/loop_practice1.java
@@ -0,0 +1,15 @@
+package lession5;
+
+public class loop_practice1 {
+    public static void main(String[] args) {
+        int a = 5;
+        for (int i = a; i > 0; i--) {
+           // for (int j = 0; j < i; j++) {
+            System.out.print("*");
+        
+        System.out.print("\n");
+        }
+    }
+    
+}
+//}
---
+++ FirstProject/src/lession5/tringle.java
@@ -0,0 +1,14 @@
+package lession5;
+
+public class tringle {
+    public static void main(String[] args) {
+        int a=4;
+        for (int i= 1; i<=a; i++) {
+            for (int j=1; j<=a; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
